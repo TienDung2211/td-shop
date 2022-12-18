06aedcf294472a76188d129b7fc516d04feb0da4
Tiến Dũng <trantiendungaaa@gmail.com>
2022-12-19 00:47:50 +0700

    Pending redux/toolkit

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyles from './components/GlobalStyles';
-import authStore from './stores/auth/authStore';
-import { Provider } from 'react-redux';
+// import authStore from './stores/auth/authStore';
+// import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={authStore}>
-        <GlobalStyles>
-            <App />
-        </GlobalStyles>
-    </Provider>,
+    // <Provider store={authStore}>
+    <GlobalStyles>
+        <App />
+    </GlobalStyles>,
+    // </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
