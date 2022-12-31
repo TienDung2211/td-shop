b7e1bdafba5f537f6e7cea0b8ba211cec4394886
Tiến Dũng <trantiendungaaa@gmail.com>
2022-12-31 08:12:22 +0700

    upload source

--- src/stores/auth/authStore.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './authSlice';
-import RootReducer from './authReducer';
-
-const authStore = configureStore({
-    reducer: RootReducer,
-});
-
-export default authStore;
