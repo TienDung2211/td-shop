3e740f4a6986fe600d214250452bf6aabb37d037
Tiến Dũng <trantiendungaaa@gmail.com>
2022-12-31 08:13:14 +0700

    upload source

--- src/index.js
+++ src/index.js
@@ -4,18 +4,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyles from './components/GlobalStyles';
 import { DataProvider } from './context/DataContext';
-// import authStore from './stores/auth/authStore';
-// import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    // <Provider store={authStore}>
     <DataProvider>
         <GlobalStyles>
             <App />
         </GlobalStyles>
     </DataProvider>,
-    // </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
